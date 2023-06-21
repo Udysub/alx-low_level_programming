b6353f01e99c881dfbdcefb138788e3138f00473
Udysub <commyjohnson72@gmail.com>
2023-06-21 15:51:46 -0700

    corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_alphabet - Prints the alphabet in lowercase
- * Return: always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-	 char letter;
+	char letter;
 
-	 for (letter = 'a'; letter <= 'z'; letter++)
-	 {
-		 _putchar(letter);
-	 }
-	 _putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+int main(void)
 {
-    int i;
-    char letter;
+	char letter;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+
+	return (0);
 }
