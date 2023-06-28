7fadef0eccc6fa92e275e2698ccdab755c8ca664
Udysub <commyjohnson72@gmail.com>
2023-06-28 08:38:47 -0700

    update task 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,53 +1,74 @@
 #include <stdio.h>
 
 /**
- * main - Finds and prints the first 98 Fibonacci numbers
- *        1 and 2, separated by comma followed by a space
+ * print_fibonacci - Prints Fibonacci numbers up to count
  *
- * Return: Always 0 (success)
+ * @count: The number of Fibonacci numbers to print
  */
-int main(void)
+void print_fibonacci(int count)
 {
-	int count;
 	unsigned long fib1 = 1, fib2 = 2, fib_next;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
 
-	for (count = 0; count <= 92; count++)
+	printf("%lu, %lu", fib1, fib2);
+
+	while (count > 2)
 	{
 		fib_next = fib1 + fib2;
-		printf("%lu, ", fib_next);
+		printf(", %lu", fib_next);
 
 		fib1 = fib2;
 		fib2 = fib_next;
+		count--;
 	}
+}
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+/**
+ * print_big_fibonacci - Prints large Fibonacci numbers from count
+ *
+ * @count: The number of large Fibonacci numbers to print
+ */
+void print_big_fibonacci(int count)
+{
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	for (count = 93; count < 99; count++)
+	fib1_half1 = 0;
+	fib1_half2 = 1;
+	fib2_half1 = 0;
+	fib2_half2 = 2;
+
+	while (count > 0)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
 
-		if (fib1_half1 + fib2_half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
 
-		printf("%lu%lu", half1, half2);
-
-		if (count != 98)
-			printf(", ");
+		printf(", %lu%010lu", half1, half2);
 
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
+		count--;
 	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 98;
+
+	print_fibonacci(count - 2);
+	print_big_fibonacci(6);
 
 	printf("\n");
 
