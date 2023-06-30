46ddd41e6681c61e23af745eea170bacf6c8e7a8
Udysub <commyjohnson72@gmail.com>
2023-06-30 13:01:53 -0700

    the next couple of task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: 1 for digit character or 0 for anything else
+ */
+
+int mul(int a, int b)
+
+{
+	int result = a * b;
+
+	return (result);
+}
